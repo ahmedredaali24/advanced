2bd39f4a5ee182e4cf0fd8e996cfe181a49aeb58
ahmedreda24 <masry5357@gmail.com>
2025-07-10 20:28:20 +0300

    added routes manager added splash screen image

--- lib/presentation/resources/style_manager.dart
+++ lib/presentation/resources/style_manager.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'font_manager.dart';
 
+
+
 TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
   return TextStyle(
     fontSize: fontSize,

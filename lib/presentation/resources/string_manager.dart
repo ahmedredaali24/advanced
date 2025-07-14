0b8cf2a878ce9ad6fb3633413fc19c86fdd76913
ahmedreda24 <masry5357@gmail.com>
2025-07-14 12:46:54 +0300

    finish design onboarding screen added more images edit theme manager file

--- lib/presentation/resources/string_manager.dart
+++ lib/presentation/resources/string_manager.dart
@@ -1,3 +1,17 @@
-class AppString{
-  static const noRoute="No Route found";
+class AppString {
+  static const noRoute = "No Route found";
+  static const skip = "skip";
+
+  static const onBoardingTitle1 = "see the best course #1";
+  static const onBoardingTitle2 = "see the best course #2";
+  static const onBoardingTitle3 = "see the best course #3";
+  static const onBoardingTitle4 = "see the best course #4";
+  static const onBoardingSupTitle1 =
+      "Tut app is an awesome flutter application using clean architecture #1";
+  static const onBoardingSupTitle2 =
+      "Tut app is an awesome flutter application using clean architecture #1";
+  static const onBoardingSupTitle3 =
+      "Tut app is an awesome flutter application using clean architecture #1";
+  static const onBoardingSupTitle4 =
+      "Tut app is an awesome flutter application using clean architecture #1";
 }
